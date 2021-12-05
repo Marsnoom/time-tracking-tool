bdfb411de15a34bf6a7a9410b280326d813bec61
Domenic Schumacher <domenic.schumacher_external@rds.de>
2021-12-05 01:09:22 +0100

    added basic component & page setup

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SiderbarComponent } from './components/siderbar/siderbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ProfileComponent,
+    NavbarComponent,
+    FooterComponent,
+    SiderbarComponent,
+    SidebarComponent,
+    LayoutComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
